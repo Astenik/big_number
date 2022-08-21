9e279d0d4102b12ccaa035f350c793ca005bd2c0
Astenik <106985461+Astenik@users.noreply.github.com>
2022-08-21 20:45:51 +0400

    Update big_number_sum.py

--- big_number_sum.py
+++ big_number_sum.py
@@ -3,8 +3,22 @@ def big_number_sum(big1: str, big2: str):
         q2 = len(big2) % 3
         lst1 = []
         lst2 = []
-        lst1.append(big1[:q1])
-        lst2.append(big2[:q2])
+        if q1 == 1:
+            lst = ['0', '0']
+            lst.append(big1[0])
+        if q1 == 2:
+            lst = ['0']
+            lst.append(big1[0])
+            lst.append(big1[1])
+        lst1.append(lst)
+        if q2 == 1:
+            lst = ['0', '0']
+            lst.append(big2[0])
+        if q2 == 2:
+            lst = ['0']
+            lst.append(big2[0])
+            lst.append(big2[1])
+        lst2.append(lst)
         i = q1
         while i < len(big1):
            count = 3
@@ -29,22 +43,18 @@ def big_number_sum(big1: str, big2: str):
             for j in range(len(lst1) - 1, -1, -1):
                 for jj in range(len(lst1[j]) - 1, -1, -1):
                       _sum = int(lst1[j][jj]) + int(lst2[j][jj]) + rem
-                      if _sum > 9:
-                         rem = 1 
-                         _sum %= 10 
-                      else:
-                         rem = 0
+                      rem = _sum // 10
+                      _sum %= 10 
                       res.append(_sum) 
             length = len(lst2) - len(lst1)
             for k in range(length, -1, -1):
                 for kk in range(len(lst2[k]) - 1, -1, -1):
                     _sum = int(lst2[k][kk]) + rem
-                    if  _sum > 9:
-                        rem = 1
-                        _sum %= 10 
-                    else:
-                        rem = 0 
-                    res.append(_sum)
+                    rem = _sum // 10
+                    _sum %= 10 
+                    res.append(_sum) 
+            if rem != 0:
+                 res.append(rem)
         elif len(lst1) > len(lst2):
             res = []
             rem = 0
@@ -95,7 +105,9 @@ def big_number_sum(big1: str, big2: str):
              num = num[::-1]
              new_res.append(num)
         res = new_res[::-1]
+        if res[0][0] == 0:
+             for num in res:
+                num.remove(num[0])
+                break
         return res 
-num1 = 2**33
-num2 = 2**33 
-print(big_number_sum(f'{num1}', f'{num2}'))
+print(big_number_sum('1374748282984393923', '892783892929292929029036'))
